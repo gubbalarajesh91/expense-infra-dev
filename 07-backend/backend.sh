428748e4fa47c0b400ddbce8d228ecd57155dfa3
gubbalarajesh91 <gubbala.rajesh91@gmail.com>
2024-06-02 16:26:06 +0530

    infra

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+component=$1
+environment=$2
 dnf install ansible -y
 pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/gubbalarajesh91/expense-ansible-roles-tf-main.git main.yaml -e component=$component -e env=$environment
--- 07-backend/main.tf
+++ 07-backend/main.tf
@@ -6,7 +6,7 @@ module "backend" {
   instance_type          = "t3.micro"
  
 
-  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
   #covert StringList to list and get first element
   subnet_id              = local.private_subnet_id
   ami = data.aws_ami.ami_info.id
@@ -20,7 +20,7 @@ module "backend" {
 }
 
 
-resource "null_resource" "cluster" {
+resource "null_resource" "backend" {
     triggers = {
         instance_id = module.backend.id # this will be triggered everytime instance created
     }
@@ -30,5 +30,18 @@ resource "null_resource" "cluster" {
     user     = "ec2-user"
     password = "DevOps321"
     host     = module.backend.private_ip
+  }
+
+  provisioner "file" {
+    source = "${var.common_tags.Component}.sh"
+    destination = "/tmp/${var.common_tags.Component}.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/${var.common_tags.Component}.sh",
+      "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component}${var.environment}",
+      
+    ]
   }
 }
